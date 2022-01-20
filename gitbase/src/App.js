da27c74a23443f06b9b3fc01d8db24676a6302cf
Dinesh Sainath <sainath.dinesh@gmail.com>
2022-01-20 12:47:20 -0500

    skeleton

---
+++ gitbase/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {SiGithub} from 'react-icons/si';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SiGithub className="gitLogo" size={60}/>
+          G I T B A S E
+      </header>
+    </div>
+  );
+}
+
+export default App;
